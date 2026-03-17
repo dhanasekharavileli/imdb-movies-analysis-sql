f717aa43c6b24df41aaa811ddc34a9e43a409de9
Dhanasekhar Avileli <dhanasekharavileli@gmail.com>
2026-03-17 09:18:47 +0530

    Add files via upload

---
+++ movies.sql
@@ -0,0 +1,61 @@
+show databases;
+use project_movie_database;
+show tables;
+DESCRIBE directors;
+DESCRIBE movies;
+SELECT * 
+FROM movies;
+SELECT * FROM directors;
+SELECT COUNT(*) AS total_movies
+FROM movies;
+SELECT *
+FROM directors
+WHERE name IN ('James Cameron', 'Luc Besson', 'John Woo');
+SELECT *
+FROM directors
+WHERE name LIKE 'S%';
+SELECT COUNT(*) AS female_directors
+FROM directors
+WHERE gender = 1;
+SELECT name
+FROM directors
+WHERE gender = 1
+ORDER BY id
+LIMIT 1 OFFSET 9;
+SELECT original_title, popularity
+FROM movies
+ORDER BY popularity DESC
+LIMIT 3;
+SELECT original_title, revenue
+FROM movies
+ORDER BY revenue DESC
+LIMIT 3;
+SELECT original_title, vote_average, release_date
+FROM movies
+WHERE release_date >= '2000-01-01'
+ORDER BY vote_average DESC
+LIMIT 1;
+SELECT m.original_title
+FROM movies m
+JOIN directors d
+  ON m.director_id = d.id
+WHERE d.name = 'Brenda Chapman';
+SELECT d.name, COUNT(m.id) AS movie_count
+FROM movies m
+JOIN directors d
+  ON m.director_id = d.id
+GROUP BY d.name
+ORDER BY movie_count DESC
+LIMIT 1;
+SELECT d.name, SUM(m.revenue) AS total_revenue
+FROM movies m
+JOIN directors d
+  ON m.director_id = d.id
+GROUP BY d.name
+ORDER BY total_revenue DESC
+LIMIT 1;
+
+
+
+
+
